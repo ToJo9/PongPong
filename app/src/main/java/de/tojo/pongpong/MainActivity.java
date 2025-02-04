3178b8bce9d88f3f2f2c0434662fca0246627a3d
ToJo <tojo1368@gmail.com>
2025-02-04 17:28:14 +0100

    Start-Button hinzugefügt (noch nicht implementiert)

--- app/src/main/java/de/tojo/pongpong/MainActivity.java
+++ app/src/main/java/de/tojo/pongpong/MainActivity.java
@@ -1,6 +1,9 @@
 package de.tojo.pongpong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(this);
+    }
+
+    //startet das Spiel
+    @Override
+    public void onClick(View view) {
+        // TODO: spiel muss gestartet werden
     }
 }
