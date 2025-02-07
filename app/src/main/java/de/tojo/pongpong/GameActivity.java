cf8a0a4c2b29b56d977e64b00277453a7d8acb16
ToJo <tojo1368@gmail.com>
2025-02-07 15:03:05 +0100

    angefangen, game engine in gameActivity zu implementieren

--- app/src/main/java/de/tojo/pongpong/GameActivity.java
+++ app/src/main/java/de/tojo/pongpong/GameActivity.java
@@ -10,6 +10,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GameActivity extends AppCompatActivity {
 
+    // TODO: default auf true oder false?
+    boolean isRunning = false;
+    int punktzahl = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,16 @@ public class GameActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        start();
+    }
+
+    private void start() {
+        isRunning = true;
+    }
+
+    private void gameOver() {
+        isRunning = false;
+        // TODO: hier passiert noch mehr
     }
 }
