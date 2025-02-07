3a9b04467e850d15af10ad283f503266e81e6e0a
ToJo <tojo1368@gmail.com>
2025-02-07 19:03:23 +0100

    Bugfix: GameActivity kann wieder gestartet werden

--- app/src/main/java/de/tojo/pongpong/GameActivity.java
+++ app/src/main/java/de/tojo/pongpong/GameActivity.java
@@ -18,8 +18,9 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(new GameView(this));
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        GameView gameView = new GameView(this);
+        setContentView(gameView);
+        ViewCompat.setOnApplyWindowInsetsListener(gameView, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/de/tojo/pongpong/GameView.java
+++ app/src/main/java/de/tojo/pongpong/GameView.java
@@ -1,11 +1,14 @@
 package de.tojo.pongpong;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.View;
 
 public class GameView extends View {
 
     public GameView(Context context) {
         super(context);
+
     }
 }
