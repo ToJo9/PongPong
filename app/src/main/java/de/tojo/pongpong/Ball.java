ec29580e6cbc321ad7b4024b1a18681bb0d67dfc
ToJo <tojo1368@gmail.com>
2025-02-13 17:44:47 +0100

    - genRandomRichtung() in Ball erstellt - MAX_ABPRALLWINKEL_GRAD in Ball erstellt (wird noch nicht verwendet)
    Ball bewegt sich beim Start jz in eine zufällige Richtung

--- app/src/main/java/de/tojo/pongpong/Ball.java
+++ app/src/main/java/de/tojo/pongpong/Ball.java
@@ -1,19 +1,26 @@
 package de.tojo.pongpong;
 
+import java.util.Random;
+
 public class Ball extends Circle {
 
-    // TODO: richtungX und richtungY sind hier erstmal nur testweise initialisiert.
-    // sie müssen random generiert werden. das passiert woanders. dabei muss beachtet werden, dass
-    // der Ball nicht immer unterschiedlich schnell startet
-    private float richtungX = 0.5f;
-    private float richtungY = 0.5f;
+    // Bei einem Abprallwinkel von 90 Grad würde der Ball orthogonal zum Paddle abprallen.
+    // Dann wäre das Spiel zu einfach.
+    // Ein Abprallwinkel von >90 Grad ist nicht möglich, weil der Ball sonst zurückspringen würde.
+    // MAX_ABPRALLWINKEL_GRAD muss also <90 sein
+    //TODO: alte berechnung zur berücksichtigung des abprallwinkels: richtungX = randomizer.nextFloat() * (1 - (2 * (WINKEL / 90))) + (WINKEL / 90);
+    private static final float MAX_ABPRALLWINKEL_GRAD = 80f;
+
+    private float richtungX;
+    private float richtungY;
     private float geschwindigkeit = 8;
     public Ball() {
-
+        genRandomRichtung();
     }
 
     public Ball(float cx, float cy, float radius) {
         super(cx, cy, radius);
+        genRandomRichtung();
     }
 
     public void move() {
@@ -21,5 +28,21 @@ public class Ball extends Circle {
         float bewegungY = richtungY * geschwindigkeit;
 
         offset(bewegungX, bewegungY, 0);
+    }
+
+    private void genRandomRichtung() {
+        Random randomizer = new Random();
+
+        // müssen zsm 1 ergeben, damit der Ball sich jedes Mal gleich schnell bewegt
+        richtungX = randomizer.nextFloat();
+        richtungY = 1 - richtungX;
+
+        if(randomizer.nextBoolean()) {
+            richtungX *= -1;
+        }
+
+        if(randomizer.nextBoolean()) {
+            richtungY *= -1;
+        }
     }
 }
